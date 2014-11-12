5f282b43ad0db4c4ab7c758bf6633bfb3f7293df
StajMap <dstajan@esri.ca>
2014-11-12 14:37:36 -0800

    removed test theme

--- CuttingsFinest/plugmyride/Code/themes/darcMapTheme/panels/SimpleBorderPanel/panel.js
+++
@@ -1,9 +0,0 @@
-define(['dojo/_base/declare',
-  'jimu/BaseWidgetPanel'
-],
-function(declare, BaseWidgetPanel) {
-
-  return declare([BaseWidgetPanel], {
-    baseClass: 'jimu-widget-panel jimu-border-panel'
-  });
-});
